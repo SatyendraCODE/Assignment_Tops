bce2af3639253e66ebcd3841a4ea139b3fcb1772
Satyendra <satyendra.code.pro@gmail.com>
2023-03-19 16:07:20 +0530

    router
    router add in ./src

--- React_Tops_project/tops_project/src/index.js
+++ React_Tops_project/tops_project/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {RouterProvider} from "react-router-dom";
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import './index.css';
+import Router from'./Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <RouterProvider router={Router} />
 );
